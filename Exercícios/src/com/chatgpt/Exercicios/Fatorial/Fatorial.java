4454178df78332e21c1af8e1f3e831bb5bfae73d
FelipeCastro2021 <felipe2021castro@gmail.com>
2024-08-22 11:18:11 -0300

    Fatorial

---
+++ Exercícios/src/com/chatgpt/Exercicios/Fatorial/Fatorial.java
@@ -0,0 +1,19 @@
+package com.chatgpt.Exercicios.Fatorial;
+
+import java.util.Scanner;
+
+public class Fatorial {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número para fazer o fatorial: ");
+		int numero = scan.nextInt();
+		int total = 1;
+		
+		for (int i = 1; i <= numero; i++) {
+			total *= i;
+		}
+		
+		System.out.println(total);
+	}
+}
